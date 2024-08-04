f374a5f0681fbb91215f35db898bc01e3dd43f97
comethrusws <basabjha@naver.com>
2024-08-05 01:04:11 +0545

    Commit #2.5: Dynamic tab title

--- app/research/page.tsx
+++ app/research/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return (
-    <main className="flex flex-col items-center justify-between px-24 py-6 mb-6">
-        Research Page
-    </main>
-  );
+    const [headerName, setHeaderName] = useState('intellectAI');
+    
+    useEffect(() => {
+        const newHeaderName = "Research | intellectAI";
+        setHeaderName(newHeaderName);
+        document.title = newHeaderName;
+    }, []);
+    
+    return (
+        <main className="flex flex-col items-center justify-between px-24 py-6 mb-6">
+            Research Page
+        </main>
+    );
 }
